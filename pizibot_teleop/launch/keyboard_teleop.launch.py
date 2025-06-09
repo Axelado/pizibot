c86850d32cb4bb5034684fa0b6e018e25251b528
Axel NIATO <axelniato@gmail.com>
2025-06-09 11:52:15 +0200

    Refactor launch description for teleop_twist_keyboard_for_azerty node and update terminal prefix

--- pizibot_teleop/launch/keyboard_teleop.launch.py
+++ pizibot_teleop/launch/keyboard_teleop.launch.py
@@ -2,15 +2,23 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    """
+    Generates a ROS 2 launch description for starting the teleop_twist_keyboard_for_azerty node.
+    This function creates a launch description that launches the teleop_twist_keyboard_for_azerty node
+    in a new xterm terminal. The node is configured to remap the '/cmd_vel' topic to '/cmd_vel_key' and
+    outputs logs to the screen.
+    Returns:
+        LaunchDescription: A ROS 2 LaunchDescription object containing the teleop keyboard node.
+    # The teleop_twist_keyboard_for_azerty node allows keyboard-based teleoperation using an AZERTY keyboard layout.
+    """
     
     teleop_keyboard_node = Node(
             package='teleop_twist_keyboard_for_azerty',
             executable='teleop_twist_keyboard_for_azerty',
             remappings=[('/cmd_vel', '/cmd_vel_key')],
             output='screen',
-            # Lancement du nœud dans un vrai terminal avec 'gnome-terminal' ou 'xterm'
-            # Choisis l'un d'eux selon le terminal installé sur ton système
-            prefix='gnome-terminal --'  # ou 'xterm -e' selon ce que tu as installé
+            
+            prefix='xterm -e' 
          )
 
     return LaunchDescription([
