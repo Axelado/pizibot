b948745cd6af4f8d8b7186cfb511ce58cf81a159
Axel NIATO <axelniato@gmail.com>
2025-06-11 11:23:59 +0200

    Fix parameter path in online async launch file for slam_toolbox node

--- pizibot_navigation/launch/online_async_launch.py
+++ pizibot_navigation/launch/online_async_launch.py
@@ -21,7 +21,7 @@ def generate_launch_description():
     declare_slam_params_file_cmd = DeclareLaunchArgument(
         'slam_params_file',
         default_value=os.path.join(get_package_share_directory(package_name),
-                                   'params', 'mapper_params_online_async.yaml'),
+                                   'param', 'mapper_params_online_async.yaml'),
         description='Full path to the ROS2 parameters file to use for the slam_toolbox node')
 
     start_async_slam_toolbox_node = Node(
