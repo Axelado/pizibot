7f86bbfa23cbfccef165c35194f97b0b6602b2f5
Axel NIATO <axelniato@gmail.com>
2025-06-12 20:49:22 +0200

    Update RPLidar launch file to include flip_x_axis parameter to invert scan

--- pizibot_hardware/launch/rplidar.launch.py
+++ pizibot_hardware/launch/rplidar.launch.py
@@ -11,11 +11,14 @@ def generate_launch_description():
             output='screen',
             parameters=[{
                 'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.4:1.0-port0', # or /dev/ttyUSB1
+                # 'serial_port': '/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0',
                 'serial_baudrate': 115200,  # A1 / A2
                 # 'serial_baudrate': 256000, # A3
                 'frame_id': 'laser_frame',
                 'inverted': False,
                 'angle_compensate': True,
+                'flip_x_axis': True,
+                
             }],
         ),
     ])
